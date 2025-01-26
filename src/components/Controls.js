1fb1ba7fb35264943d09d5527c2618a9ae8ee039
Ru2 <wretchray@gmail.com>
2025-01-26 09:30:30 -0500

    Checkpoint ( more css to make it more pretty and even)

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Controls ({ gobBack, goFoward, clearHistory, backDisabled, forwardDisabled }) {
+function Controls ({ goBack, goForward, clearHistory, backDisabled, forwardDisabled }) {
     return (
         <div className="Controls">
-            <button onClick={gobBack} disabled={backDisabled}>Back</button>
-            <button onClick={goFoward} disabled={forwardDisabled}>Forward</button>
+            <button onClick={goBack} disabled={backDisabled}>Back</button>
+            <button onClick={goForward} disabled={forwardDisabled}>Forward</button>
             <button onClick={clearHistory}>Clear History</button>
         </div>
     );
--- src/components/HistoryDisplay.js
+++ src/components/HistoryDisplay.js
@@ -3,26 +3,25 @@ import React from "react";
 function HistoryDisplay({ backStack, forwardStack, navigateBack, navigateForward }) {
     return (
         <div className="history-display">
-            <h3>Back Stack</h3>
-            <ul className="history-list">
+            <div className="back-stack">
+                <h3>Back Stack</h3>
+                <ul className="history-list">
                     {backStack.map((page, index) => (
                         <li key={index}>{page}</li>
                     ))}
                 </ul>
-            <button onClick={navigateBack} disabled={backStack.length === 0}>
-                Back
-            </button>
-            <h3>Forward Stack</h3>
-            <ul className="history-list">
+            </div>
+
+            <div className="forward-stack">
+                <h3>Forward Stack</h3>
+                <ul className="history-list">
                     {forwardStack.map((page, index) => (
                         <li key={index}>{page}</li>
                     ))}
                 </ul>
-            <button onClick={navigateForward} disabled={forwardStack.length === 0}>
-                Forward
-            </button>
+            </div>
         </div>
     );
 }
 
-export default HistoryDisplay
+export default HistoryDisplay;
